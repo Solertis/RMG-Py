cd2fa7854dd4f65052c7896efc0a485577762479
alongd <alongd@mit.edu>
2017-11-22 21:20:20 -0500

    fixup! charged atomTypes in group.py

--- rmgpy/molecule/group.py
+++ rmgpy/molecule/group.py
@@ -1984,13 +1984,13 @@ class Group(Graph):
                 else:
                     raise UnexpectedChargeError(graph = newMolecule)
                 #check hardcoded atomtypes
-                positiveCharged = ['Csc','Cdc','Ctc',
-                                   'N3dc','N5sc','N5dc','N5ddc','N5tc',
-                                   'O2sp','O4sc','O4dc','O4tc',
+                positiveCharged = ['Csc','Cdc',
+                                   'N3sc','N5sc','N5dc','N5ddc','N5tc','N5b',
+                                   'O2sc','O4sc','O4dc','O4tc',
                                    'S2sc','S4sc','S4dc','S4tdc','S6sc','S6dc','S6tdc']
-                negativeCharged = ['Csc','Cdc','Ctc','C2sc','C2dc','C2tc',
-                                   'N0sc','N1sc','N1dc','N3dc','N3tdc','N5dddc','N5tdc','N5b',
-                                   'O0sc','O2sn','O2dc','O2tc',
+                negativeCharged = ['C2sc','C2dc','C2tc',
+                                   'N0sc','N1sc','N1dc','N5dddc',
+                                   'O0sc',
                                    'S0sc','S2sc','S2dc','S2tc','S4dc','S4tdc','S6sc','S6dc','S6tdc']
                 if groupAtom.atomType[0] in [atomTypes[x] for x in positiveCharged] and atom.charge > 0:
                     pass
